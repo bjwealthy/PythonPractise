4bf6a2a8be12eef398dbec8b6b97245d0944a612
mobolaji <bjwealthy@gmail.com>
2022-06-02 07:29:10 +0100

    second commit

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
 print('Hello world!')
+squares=[1,4,9,16,24, 36, 49, 64, 81, 100, 121, 144, 169]
+print(squares)
+#indexes
+print(squares[1])
+print(squares[-1])
+#slice
+print(squares[-3:])
+print(squares[:])
+#concatenation
+squares=squares+[196,225,256]
+print(squares)
+#mutation
+squares[4]=25
+print(squares)
+
